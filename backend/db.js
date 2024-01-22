d2f65d5bc84ee1271ba33dcf0a85acc1f4042854
Shubham1193 <shubhamsingh91930@gmail.com>
2024-01-22 21:28:22 +0530

    fontendmodified

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 // mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos
 // .env
+//shubham
 mongoose.connect("mongodb+srv://admin:Shubham1173@cluster0.8uttq.mongodb.net/todos")
 const todoSchema = mongoose.Schema({
     title: String,
